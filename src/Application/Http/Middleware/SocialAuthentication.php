77579ceec2a6a418c5d4d141db1e8110d8c6367e
steph bug <authters@gmail.com>
2019-07-23 18:25:25 +0200

    refactor social authentication to a unique middleware

--- src/Application/Http/Middleware/SocialAuthentication.php
+++ src/Application/Http/Middleware/SocialAuthentication.php
@@ -6,34 +6,100 @@ use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 use InvalidArgumentException;
 use Laravel\Socialite\Two\InvalidStateException;
+use MerchantOfComplexity\Authters\Application\Http\Response\SocialProviderEntrypoint;
 use MerchantOfComplexity\Authters\Firewall\Guard\HasEventGuard;
 use MerchantOfComplexity\Authters\Guard\Authentication\Authenticator\SocialAuthenticator;
 use MerchantOfComplexity\Authters\Support\Contract\Application\Http\Middleware\AuthenticationEventGuard;
 use MerchantOfComplexity\Authters\Support\Contract\Firewall\Key\ContextKey;
 use MerchantOfComplexity\Authters\Support\Exception\AuthenticationException;
+use MerchantOfComplexity\Authters\Support\Exception\IdentityNotFound;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 use function get_class;
 
-abstract class SocialAuthentication extends Authentication implements AuthenticationEventGuard
+final class SocialAuthentication extends Authentication implements AuthenticationEventGuard
 {
     use HasEventGuard;
 
     /**
      * @var SocialAuthenticator
      */
-    protected $authenticator;
+    private $authenticator;
+
+    /**
+     * @var SocialProviderEntrypoint
+     */
+    private $entrypoint;
 
     /**
      * @var ContextKey
      */
-    protected $contextKey;
+    private $contextKey;
 
-    public function __construct(SocialAuthenticator $authenticator, ContextKey $contextKey)
+    public function __construct(SocialAuthenticator $authenticator,
+                                SocialProviderEntrypoint $entrypoint,
+                                ContextKey $contextKey)
     {
         $this->authenticator = $authenticator;
+        $this->entrypoint = $entrypoint;
         $this->contextKey = $contextKey;
     }
+
+    protected function processAuthentication(Request $request): ?Response
+    {
+        if ($this->authenticator->socialRequest()->isRedirect($request)) {
+            return $this->handleRedirectRequest($request);
+        }
+
+        return $this->handleLoginRequest($request);
+    }
+
+    protected function handleRedirectRequest(Request $request): Response
+    {
+        try {
+            // first we create a token with a "need registration" role
+            // if we succeed to authenticate the token, identity has been already registered
+            // we store a new token with a "login" role
+            // we keep workflow in both case to be handled in a endpoint
+            $token = $this->authenticator->createRegistrationSocialToken($request, $this->contextKey);
+
+            $this->fireAttemptLoginEvent($request, $token);
+
+            try {
+                $token = $this->authenticator->createLoginSocialToken(
+                    $this->guard->authenticateToken($token)
+                );
+            } catch (IdentityNotFound $notFound) {
+                //
+            }
+
+            $this->fireSuccessLoginEvent($request, $token);
+
+            $this->guard->storage()->setToken($token);
+
+            return null;
+        } catch (Throwable $exception) {
+            return $this->handleAuthenticationFailure($request, $exception);
+        }
+    }
+
+    protected function handleLoginRequest(Request $request): Response
+    {
+        try {
+            $this->authenticator->extractProviderName($request);
+
+            return $this->entrypoint->startAuthentication($request);
+        } catch (Throwable $exception) {
+            return $this->handleAuthenticationFailure($request, $exception);
+        }
+    }
+
+    protected function requireAuthentication(Request $request): bool
+    {
+        return $this->guard->isStorageEmpty()
+            && $this->authenticator->socialRequest()->match($request);
+    }
+
 
     /**
      * @param Request $request
@@ -56,12 +122,7 @@ abstract class SocialAuthentication extends Authentication implements Authentica
         throw $exception;
     }
 
-    /**
-     * @param Throwable $exception
-     * @return AuthenticationException
-     * @throws Throwable
-     */
-    private function handleAuthenticationException(Throwable $exception): ?AuthenticationException
+    protected function handleAuthenticationException(Throwable $exception): ?AuthenticationException
     {
         if ($exception instanceof AuthenticationException) {
             return $exception;
