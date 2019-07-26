fc4b073a7e3aaa99ff1552a28b93fa4212a3e984
steph bug <authters@gmail.com>
2019-07-26 11:05:28 +0200

    minor

--- src/Support/Contract/Guard/ModelIdentifier.php
+++ src/Support/Contract/Guard/ModelIdentifier.php
@@ -3,8 +3,9 @@
 namespace MerchantOfComplexity\Authters\Support\Contract\Guard;
 
 use MerchantOfComplexity\Authters\Support\Contract\Domain\Identity;
+use Serializable;
 
-interface ModelIdentifier extends Identity, \Serializable
+interface ModelIdentifier extends Identity, Serializable
 {
     public function newIdentityModelInstance(): ?Identity;
 
